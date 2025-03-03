6432f890173a43d1ce54c3d60f0d4b818229376d
rabab003 <bintaliraq4@gmail.com>
2025-03-03 17:29:17 +0300

    3/3 started new Porject with react js

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+
+export default function Home(){
+    return(
+        <div>
+            <h2>hello</h2>
+            <HomeRoundedIcon/>
+
+        </div>
+    )
+}
